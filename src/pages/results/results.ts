3480fb8b3a2eff4661695783941e2fb0120cff95
airtorres <komsaira2015@gmail.com>
2018-11-11 12:21:33 +0800

    Added hardcoded sample Google PieChart and BarChart

--- src/pages/results/results.ts
+++ src/pages/results/results.ts
@@ -8,6 +8,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
+ declare var google;
+
 @IonicPage()
 @Component({
   selector: 'page-results',
@@ -20,6 +22,64 @@ export class ResultsPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ResultsPage');
+  }
+
+  showPieChart(){
+  	// Create the data table.
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Topping');
+    data.addColumn('number', 'Slices');
+    data.addRows([
+      ['Mushrooms', 3],
+      ['Onions', 1],
+      ['Olives', 1],
+      ['Zucchini', 1],
+      ['Pepperoni', 2]
+    ]);
+
+    // Set chart options
+    var options = {
+    	'title':'1. How Much Pizza I Ate Last Night?',
+        'width':400,
+        'height':300
+	};
+
+    // Instantiate and draw our chart, passing in some options.
+    var chart = new google.visualization.PieChart(document.getElementById('pie_chart_div'));
+    chart.draw(data, options);
+  }
+
+  showBarChart(){
+  	// Create the data table.
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Topping');
+    data.addColumn('number', 'Slices');
+    data.addRows([
+      ['Mushrooms', 3],
+      ['Onions', 1],
+      ['Olives', 1],
+      ['Zucchini', 1],
+      ['Pepperoni', 2]
+    ]);
+
+    // Set chart options
+    var options = {
+    	'title':'1. How Much Pizza I Ate Last Night?',
+        'width':400,
+        'height':300,
+	};
+
+    // Instantiate and draw our chart, passing in some options.
+    var chart = new google.visualization.BarChart(document.getElementById('bar_chartdiv'));
+    chart.draw(data, options);
+  }
+
+  public ionViewWillEnter(){
+  	console.log("loading charts ...");
+  }
+
+  public ionViewWillLeave(){
+  	console.log("leaving results page ...");
   }
 
 }
