fda417544ba567e86809a0bc77a5e135ec4b0824
airtorres <komsaira2015@gmail.com>
2019-01-28 01:24:39 +0800

    Fetching Surveylist from firebase

--- src/pages/survey-list/survey-list.ts
+++ src/pages/survey-list/survey-list.ts
@@ -4,6 +4,9 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { SurveySummaryPage } from '../survey-summary/survey-summary';
 import { AnswerSurveyPage } from '../answer-survey/answer-survey';
 
+import { AngularFireAuth } from '@angular/fire/auth';
+import * as firebase from 'firebase/app';
+import 'firebase/database';
 import { Storage } from '@ionic/storage';
 
 /**
@@ -37,71 +40,16 @@ export class SurveyListPage {
   all_surveys = [];
 	
   constructor(public navCtrl: NavController, public navParams: NavParams,
+    private fire: AngularFireAuth,
     private storage: Storage) {
 
     this.storage.get('currentUser').then(x =>{
       this.currUser = x;
     });
-
-    // this.updateList();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SurveyListPage');
-  }
-
-  updateList(){
-    this.storage.ready().then(() => {
-      this.storage.get("surveys").then(value => {
-        this.surveys = value;
-      
-        this.storage.get('users').then(u =>{
-          for ( var i in u['users']){
-            if (u['users'][i]['email'] == this.currUser){
-              this.mySurveys_ids = u['users'][i]['surveys'];
-
-              var invitations = u['users'][i]['invitations'];
-              for ( var inv in invitations){
-
-                var idx = u['users'][i]['invitations'][inv]['s_id'];
-                this.invite_status[idx] = '';
-                this.invite_status[idx] = u['users'][i]['invitations'][inv]['status'];
-                this.survey_invites_ids.push(u['users'][i]['invitations'][inv]['s_id']);
-              }
-
-              console.log(this.mySurveys_ids);
-              console.log(this.survey_invites_ids);
-            }
-          }
-
-          var id;
-          for( var x in this.mySurveys_ids){
-            // console.log(this.surveys['surveys'][id]);
-            id = this.mySurveys_ids[x];
-            if(this.surveys['surveys'][id]){
-              this.surveys['surveys'][id]['id'] = '';
-              this.surveys['surveys'][id]['id'] = id; // id is needed to be passed later
-              this.surveys['surveys'][id]['type'] = '';
-              this.surveys['surveys'][id]['type'] = 'mySurvey';
-              this.mySurveys.push(this.surveys['surveys'][id]);
-              this.all_surveys.push(this.surveys['surveys'][id]);
-            }
-          }
-
-          for ( var y in this.survey_invites_ids){
-            id = this.survey_invites_ids[y];
-            if(this.surveys['surveys'][id]){
-              this.surveys['surveys'][id]['id'] = '';
-              this.surveys['surveys'][id]['id'] = id; // id is needed to be passed later
-              this.surveys['surveys'][id]['type'] = '';
-              this.surveys['surveys'][id]['type'] = 'invites';
-              this.survey_invites.push(this.surveys['surveys'][id]);
-              this.all_surveys.push(this.surveys['surveys'][id]);
-            }
-          }
-        });
-      });
-    });
   }
 
   gotoSummary(item){
@@ -110,6 +58,82 @@ export class SurveyListPage {
 
   gotoRespondentView(item){
     this.navCtrl.push(AnswerSurveyPage, {'item' : item});
+  }
+
+  loadSurveysFromLocalDB(){
+    this.storage.get("mySurveys").then(mySurv => {
+        this.mySurveys = mySurv;
+    });
+    this.storage.get("survey_invites").then(invites => {
+        this.survey_invites = invites;
+    });
+    this.storage.get("all_surveys").then(all => {
+        this.all_surveys = all;
+    });
+  }
+
+  fetchSurveys(){
+    // fetch mysurveys from firebase
+    try{
+      var user_to_survey = [];
+      const userSurveyRef:firebase.database.Reference = firebase.database().ref('/user_surveys');
+      userSurveyRef.on('value', userToSurveySnapshot => {
+        user_to_survey = userToSurveySnapshot.val();
+      });
+
+      // fetching mySurveys IDs
+      for (var usr in user_to_survey){
+        if (this.fire.auth.currentUser.email == user_to_survey[usr]['email']){
+          if(user_to_survey[usr]['surveylist']){
+            this.mySurveys_ids = user_to_survey[usr]['surveylist'];
+          }
+        }
+      }
+      // fetching mySurveyInvitations IDs
+      for (var inv in user_to_survey){
+        if (this.fire.auth.currentUser.email == user_to_survey[inv]['email']){
+          if(user_to_survey[inv]['invitations']){
+            this.survey_invites_ids = user_to_survey[inv]['invitations'];
+          }
+        }
+      }
+
+      var i;
+      var temp = {};
+      for(i in this.mySurveys_ids){
+        const mysurv:firebase.database.Reference = firebase.database().ref('/surveys/'+this.mySurveys_ids[i]);
+        mysurv.on('value', mysurvSnapshot => {
+          temp = mysurvSnapshot.val();
+          temp['id'] = '';
+          temp['id'] = this.mySurveys_ids[i];
+          temp['type'] = '';
+          temp['type'] = 'mySurvey';
+          this.mySurveys.push(temp);
+          this.all_surveys.push(temp);
+        });
+      }
+
+      for(i in this.survey_invites_ids){
+        const mysurv:firebase.database.Reference = firebase.database().ref('/surveys/'+this.survey_invites_ids[i]);
+        mysurv.on('value', mysurvSnapshot => {
+          temp = mysurvSnapshot.val();
+          temp['id'] = '';
+          temp['id'] = this.survey_invites_ids[i];
+          temp['type'] = '';
+          temp['type'] = 'invites';
+          this.survey_invites.push(temp);
+          this.all_surveys.push(temp);
+        });
+      }
+
+      // saving surveys to local storage for offline access
+      this.storage.set('mySurveys', this.mySurveys);
+      this.storage.set('survey_invites', this.survey_invites);
+      this.storage.set('all_surveys', this.all_surveys);
+    }catch(e){
+      console.log("Error occurs while fetching survey list.");
+      this.loadSurveysFromLocalDB();
+    }
   }
 
   public ionViewWillEnter() {
@@ -125,8 +149,7 @@ export class SurveyListPage {
 
     // mySurveys + survey invites
     this.all_surveys = [];
-
-    this.updateList();
+    this.fetchSurveys();
   }
 
 }
